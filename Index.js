e3a0e138e5e84b6a4d4deaa0933b45ab71a2f819
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2024-05-02 13:45:23 +0200

    ye

--- Index.js
+++ Index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = express.Router();
+router.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(3000, () => console.log(`Server is listening on port 3000`));
+app.use('/', router);
+
 async function SumbitData() {
 	let it = 0;
 	var all = [];
