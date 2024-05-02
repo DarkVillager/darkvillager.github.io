0a19dda9347815f1d186c19b0b9298d3f7b9c671
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2024-05-02 14:10:06 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,7 +1,18 @@
 const fs = require('@cyclic.sh/s3fs')(process.env.CYCLIC_BUCKET_NAME);
+const Path = require('path');
 const express = require('express');
 const app = express();
 const port = 80;
+
+function ThroughDirectory(Directory) {
+	fs.readdirSync(Directory).forEach((File) => {
+		const Absolute = Path.join(Directory, File);
+		if (FS.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+		else return Files.push(Absolute);
+	});
+}
+
+ThroughDirectory('./');
 
 app.get('/', function (req, res) {
 	res.sendFile(fs.readFileSync('index.html').toString());
