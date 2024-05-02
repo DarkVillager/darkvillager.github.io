13238e39d0eae2a8a696cb54d10427bc017b0690
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2024-05-02 13:49:41 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 const router = express.Router();
 router.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile('index.html');
 });
-app.listen(() => console.log(`Server is listening on port 3000`));
+app.listen(3000, () => console.log(`Server is listening on port 3000`));
 app.use('/', router);
 
 async function SumbitData() {
